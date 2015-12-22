a2d0faa613a9939ff3ec754783b13357c57cb8a4
lhhan.liu <lhhan.liu@qunar.com>
2015-12-22 17:48:25 +0800

    12月22日

--- test.c
+++ test.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 //@all 返回1说明成功
 typedef struct {
-	int date[10];
+	int data[10];
 	int length;
 } testList;
 
@@ -10,22 +10,49 @@ int initList(testList *L) {
 	return 1;
 }
 int getElement(testList L, int i, int *e) {
-	if (L.length == 0 || i < 1 || i >10) {
+	if (L.length == 0 || i < 1 || i >L.length) {
 		return 0;
 	}
 
-	*e = L.date[i - 1];
+	*e = L.data[i - 1];
 	return 1;
 
 }
 int addElement(testList *L, int position, int i) {
-	
+	if (L->length >= 10|| position < 1 || position > L->length+1) {
+		return 0;
+	}
+	L->length = L->length + 1;
+	if (position <= L->length) {
+		for (int j = L->length; j >= position; j--) {
+			L->data[j] = L->data[j - 1];
+		}
+
+		
+	}
+	L->data[position - 1] = i;
+
+	return 1;
+}
+
+int deleteElement(testList *L, int position, int *e) {
+	if (L->length == 0 || position<1 || position>L->length) {
+		return 0;
+	}
+	*e = L->data[position - 1];
+	if (position < L->length) {
+		for (int i = position - 1; i <= L->length - 2; i++) {
+			L->data[i] = L->data[i + 1];
+		}
+	}
+	L->length = L->length - 1;
+
 }
 
 int main() {
 	int *point;
 	int value = 1;
-
+	
 	point = &value;
 
 	printf("%d\n", value);
